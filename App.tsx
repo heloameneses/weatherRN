1b7b4d8b81bbc806067ea144e7e45b8405a9792c
Heloá Meneses <heloa.meneses@gmail.com>
2020-02-02 16:28:11 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
